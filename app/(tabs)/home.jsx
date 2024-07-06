19d9976537210cd45913dd804f18648820230509
asuselessbrain <shovon1023456789@gmail.com>
2024-07-07 01:05:20 +0600

    outfit font added

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text style={{fontSize: 40,fontFamily: 'outfit-bold'}}>Home</Text>
     </View>
   )
 }
