e4e4489c0db24f0276262eb0a1fd31f61cadb6eb
asuselessbrain <shovon1023456789@gmail.com>
2024-07-07 00:49:48 +0600

    navigation complete and design nav bar

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{headerShown:false}}>
+        <Tabs.Screen name='home' options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color})=><Ionicons name="home" size={24} color={color} />
+        }} />
+        <Tabs.Screen name='explore' options={{
+            tabBarLabel: 'Explore',
+            tabBarIcon: ({color})=><Ionicons name="search" size={24} color={color} />
+        }} />
+        <Tabs.Screen name='profile' options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({color})=><Ionicons name="people-circle" size={24} color={color} />
+        }} />
+    </Tabs>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <Redirect href={'/home'} />
 }
