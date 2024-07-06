77e0686bab0eb60d1d9f4654683223d7c8e1fff7
asuselessbrain <shovon1023456789@gmail.com>
2024-07-07 01:39:52 +0600

    signIn and signOut is emplemented

---
+++ app/_layout.jsx
@@ -0,0 +1,30 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function RootLayout() {
+  useFonts({
+    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <SignedIn>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <Text style={{fontSize:40}}>Sign Out</Text>
+      </SignedOut>
+    </ClerkProvider>
+  );
+}
